7583074822e439c95d48916afea4a052830d803f
kyps <smurfdufutur1@gmail.com>
2016-11-24 20:32:58 +0100

    Add files via upload

---
+++ MetaHour/MetaHour/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using LeagueSharp;
+using System.Drawing;
+
+namespace MetaHour {
+    class Program {
+
+        private static void Main(string[] args) {
+            Drawing.OnDraw += Game_OnDraw;
+            Game.PrintChat("<font color='#F5D76E'>Loaded MetaHour by kyps.</font>");
+        }
+
+        private static void Game_OnDraw(EventArgs args) {
+            Drawing.DrawText(Drawing.Width - (Drawing.Width / 3), 10, Color.White, "" + DateTime.Now);
+        }
+
+    }
+}
---
+++ MetaSmite/ConsoleApplication1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace MetaSmite {
+    class Program {
+        private static Obj_AI_Minion Minion;
+        public static Obj_AI_Hero Player => ObjectManager.Player;
+        public static Spell SmiteSpell;
+
+        private static void Main(string[] args) {
+            try {
+                var efslot = Player.Spellbook.Spells.FirstOrDefault(x => x.Name.ToLower().Contains("smite"));
+                if (efslot != null) {
+                    Game.PrintChat("<font color='#F5D76E'>Loaded MetaSmite by kyps.</font>");
+                    SmiteSpell = new Spell(efslot.Slot, 600, TargetSelector.DamageType.True);
+                }
+            } catch (Exception e) {
+                Console.WriteLine("Error: '{0}'", e);
+            }
+            Game.OnUpdate += Game_OnTick;
+        }
+
+        private static void Game_OnTick(EventArgs args) {
+            try {
+                if (Player.IsDead) {
+                    return;
+                }
+                Minion = (Obj_AI_Minion) MinionManager.GetMinions(Player.ServerPosition, 600f, MinionTypes.All, MinionTeam.Neutral, MinionOrderTypes.MaxHealth).FirstOrDefault();
+                if (Minion == null) {
+                    return;
+                }
+                if (Minion.MaxHealth < 4900) {
+                    return;
+                }
+                if (SmiteSpell.IsReady()) {
+                    int[] damages = { 20 * Player.Level + 370, 30 * Player.Level + 330, 40 * Player.Level + 240, 50 * Player.Level + 100 };
+                    if (damages.Max() >= Minion.Health && SmiteSpell.CanCast(Minion)) {
+                        Player.Spellbook.CastSpell(SmiteSpell.Slot, Minion);
+                    }
+                }
+            } catch (Exception e) {
+                Console.WriteLine("Error: '{0}'", e);
+            }
+        }
+    }
+}
